3c9c46599d30e9c83a057926645cee9e0ca4b1fb
ofiri <ofiri112@gmail.com>
2022-05-04 23:21:48 +0300

    4.5

--- str.cpp
+++ str.cpp
@@ -3,12 +3,13 @@
 #include "iostream"
 //constructor
 Str::Str() {
-    m_str = strdup("none");
+   m_str = strdup("none");
 }
 
 //constructor
 Str::Str(const Str &other) {
-    m_str = strdup(other.m_str);
+    m_str = nullptr;
+    *this = other;
 }
 
 //constructor
@@ -58,24 +59,31 @@ bool Str::operator<(const Str &other) const {
 const Str &Str::operator=(const Str &other) {
     //if they are not pointing to the same string
     if (this != &other) {
-        if(m_str!= nullptr)
+        if(m_str!= nullptr) {
             delete[] m_str;
-        m_str= strdup(other.getStr());
+        }
+        m_str = strdup(other.getStr());
     }
     return *this;
 }
 
-//using the last = operator by creating Str object from the given string
+//delete the old m_str and update it to be the given str
 const Str &Str::operator=(const char *str) {
-    Str str1(str);
-    *this = str1;
-    return *this;
+   if(&m_str!=&str){
+       if(m_str!= nullptr) {
+           delete[] m_str;
+       }
+       m_str= strdup(str);
+   }
+   return *this;
 }
 
 //return the m_str[index]
 char &Str::operator[](int index) const {
-//    if(index>=(int) strlen(m_str)||index<0)
-//        exit(0);
+    if(index>=(int)strlen(m_str)||index<0){
+        cout<<"error"<<endl;
+        exit(0);
+    }
     return m_str[index];
 }
 
@@ -115,15 +123,11 @@ int Str::operator()(char ch) const {
 Str Str::operator+(const Str &other) const {
     int size = (int) strlen(m_str);
     int otherSize = (int) strlen(other.getStr());
-    Str concatStings = new char[size + otherSize + 1];
-    for (int i = 0; i < size; ++i) {
-        concatStings[i] = m_str[i];
-    }
-    for (int i = 0; i < otherSize; ++i) {
-        concatStings[size + i] = other[i];
-    }
-    concatStings[size + otherSize] = '\0';
-    return concatStings;
+    char* concatStings = new char[size + otherSize];
+    strcpy(concatStings, m_str);
+    strcat(concatStings, other.getStr());
+    Str strConcat(concatStings);
+    return strConcat;
 }
 
 //concatenates between current's m_str and other's m_str.
@@ -141,28 +145,18 @@ Str::operator int() const {
 
 //concatenates between str and other's m_str
 Str operator+(const char *str, const Str &other) {
-    int size = (int) strlen(str);
-    int otherSize = (int) strlen(other.getStr());
-    Str concatStings = new char[size + otherSize + 1];
-    for (int i = 0; i < size; ++i) {
-        concatStings[i] = str[i];
-    }
-    for (int i = 0; i < otherSize; ++i) {
-        concatStings[size + i] = other[i];
-    }
-    concatStings[size + otherSize] = '\0';
-    return concatStings;
+    Str str1(str);
+    return (str1+other);
 }
 
 //concatenates the given Str num times
 Str operator*(int num, const Str &other) {
     int otherSize = (int) strlen(other.getStr());
-    Str concatStings = new char[num * otherSize + 1];
-    for (int j = 0; j < num; ++j) {
-        for (int i = 0; i < otherSize; ++i) {
-            concatStings[j * otherSize + i] = other[i];
-        }
+    char* concatStings = new char[num * otherSize];
+    strcpy(concatStings, other.getStr());
+    for (int j = 1; j < num; ++j) {
+        strcat(concatStings, other.getStr());
     }
-    concatStings[num * otherSize] = '\0';
-    return concatStings;
+    Str str1(concatStings);
+    return str1;
 }
